8beec571acf12b6ce6ef1519f963b3c7166d7556
Dario George <edu.dariogeorge21@gmail.com>
2025-02-11 07:24:28 +0530

    Update small-large-element in array.c

--- small-large-element in array.c
+++ small-large-element in array.c
@@ -9,16 +9,18 @@ int main(){
     for(int i=0; i<n; i++){
         scanf("%d", &arr[i]);
     }
-    int small = arr[0], large = arr[0];
+    int small = arr[0], large = arr[0],indexL,indexS;
     for(int i=1; i<n; i++){
         if(arr[i] < small){
             small = arr[i];
+            indexS=i;
         }
         if(arr[i] > large){
             large = arr[i];
+            indexL=i;
         }
     }
-    printf("The smallest element in the array is: %d\n", small);
-    printf("The largest element in the array is: %d\n", large);
+    printf("The smallest element at index %d in the array is: %d\n",indexS, small);
+    printf("The largest element at index %d in the array is: %d\n",indexL, large);
     return 0;
 }
