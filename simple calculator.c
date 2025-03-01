182782a0f8f74da102d081b4849fbe55f6897266
Dario George <edu.dariogeorge21@gmail.com>
2025-03-02 01:32:30 +0530

    Fixed

--- simple calculator.c
+++ simple calculator.c
@@ -22,10 +22,17 @@ int main(){
             printf("Result: %.2lf\n", (float)num1 / num2);
         }else{
             printf("Error! Division by zero is not allowed.\n");
-    } 
-    } else {
-        printf("Invalid operator!\n");
-    }
+
+        }
+    } else if (operator == '%') {
+        if (num2 != 0) {
+            printf("Result: %.2lf\n", (float)((int)num1 % (int)num2));
+        }else{
+            printf("Error! Division by zero is not allowed.\n");
+        }
     
-    return 0;
+    }
+     else {
+        printf("Invalid operator!\n");
+    }return 0;
 }
