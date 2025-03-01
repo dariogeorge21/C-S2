471eb4840477eb005decbf657b30d9e27a18b7f2
Dario George <edu.dariogeorge21@gmail.com>
2025-03-02 01:54:02 +0530

    Fixed

--- number odd even check.c
+++ number odd even check.c
@@ -4,6 +4,6 @@ int main(){
     int num;
     printf("Enter a number: ");
     scanf("%d", &num);
-    num%2==0 ? printf("The number is even") : printf("The number is odd");
+    num%2==0 ? printf("The number is even") : printf("The number is odd"); //ternary operator or conditional operator
     return 0;
 }
