d41a98d6565d20fbdf2037a0176aaa365dbb6ba4
Dario George <edu.dariogeorge21@gmail.com>
2025-03-01 21:12:17 +0530

    Fixed

--- sum of n numbers.c
+++ sum of n numbers.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+// Online C compiler to run C program online
+#include <stdio.h>
 
-int main(){
-    int n, sum = 0;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-    for(int i = 1; i <= n; i++){
-        sum += i;
+int main() {
+    int limit,sum=0;
+    printf("Enter limit: ");
+    scanf("%d",&limit);
+    for(int i=1;i<=limit;i++){
+        sum+=i;
     }
-    printf("Sum of first %d numbers is %d", n, sum);
+    printf("Sum is: %d\n",sum);
     return 0;
 }
