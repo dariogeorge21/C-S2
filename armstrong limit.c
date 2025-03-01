5c0655bf9a3f699d985b1d09a41fcbcffa6f978b
Dario George <edu.dariogeorge21@gmail.com>
2025-03-01 21:04:17 +0530

    Fixed

--- armstrong limit.c
+++ armstrong limit.c
@@ -1,31 +1,29 @@
-#include<stdio.h>
+// Online C compiler to run C program online
+#include <stdio.h>
 #include<math.h>
-
-int main(){
-    int limit, arm_num, tempNum;
-    printf("Enter an integer: ");
-    scanf("%d", &limit);
-    for (int i = 1; i <=limit; i++){
-        int n = i;
-        int len = 0;
-        int sum = 0;
-        while(n != 0){
-            len++;
-            n /= 10;
+int main() {
+    int limit;
+    printf("Enter the limit: ");
+    scanf("%d",&limit);
+    for (int num=1;num<=limit;num++){
+        int tempNum,length=0,armNum=0;
+        tempNum=num;
+        while (tempNum!=0){
+            length++;
+            tempNum/=10;
         }
-        n = i;
-        while(n != 0){
-            arm_num = n % 10;
-            sum += pow(arm_num, len);
-            n /= 10;
+        tempNum=num;
+        while (tempNum!=0){
+            int digit=tempNum%10;
+            armNum+=(pow(digit,length));
+            tempNum/=10;
         }
-        if (sum == i){
-            printf("%d is an Armstrong number.\n", i);
-        }else{
-             printf("%d is not an Armstrong number.\n", i);
+        if (armNum==num){
+            printf("%d is Armstrong!!\n",num);
+        }
+        else{
+            printf("%d is not armstrong!!\n",num);
         }
     }
-    
-
     return 0;
 }
