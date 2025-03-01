a0b78696bb61a1aebee2cca5b69617acd60ec3b6
Dario George <edu.dariogeorge21@gmail.com>
2025-03-01 20:59:17 +0530

    Fixed

--- armstrong.c
+++ armstrong.c
@@ -1,26 +1,26 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    int n, arm_num=0, tempNum;
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-    tempNum = n;
-    int len=0;
-    while(tempNum != 0){
-        len++;
-        tempNum /= 10;
-    }
-    tempNum = n;
-    while(tempNum != 0){
-        int digit = tempNum % 10;
-        arm_num += pow(digit, len);
-        tempNum /= 10;
-    }
-    if (arm_num==n){
-        printf("%d is an Armstrong number.", n);
-    }else{
-        printf("%d is not an Armstrong number.", n);
-    }
 
+#include <stdio.h>
+#include<math.h>
+int main() {
+    int num,tempNum,length=0,armNum=0;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+    tempNum=num;
+    while (tempNum!=0){
+        length++;
+        tempNum/=10;
+    }
+    tempNum=num;
+    while (tempNum!=0){
+        int digit=tempNum%10;
+        armNum+=(pow(digit,length));
+        tempNum/=10;
+    }
+    if (armNum==num){
+        printf("%d is Armstrong!!",num);
+    }
+    else{
+        printf("%d is not armstrong!!",num);
+    }
     return 0;
 }
