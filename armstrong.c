4b82917035eec0f2436658f3babcc7ad761712f2
Dario George <edu.dariogeorge21@gmail.com>
2025-03-01 21:03:45 +0530

    Fixed

--- armstrong.c
+++ armstrong.c
@@ -1,26 +1,29 @@
-
+// Online C compiler to run C program online
 #include <stdio.h>
 #include<math.h>
 int main() {
-    int num,tempNum,length=0,armNum=0;
-    printf("Enter the number: ");
-    scanf("%d",&num);
-    tempNum=num;
-    while (tempNum!=0){
-        length++;
-        tempNum/=10;
-    }
-    tempNum=num;
-    while (tempNum!=0){
-        int digit=tempNum%10;
-        armNum+=(pow(digit,length));
-        tempNum/=10;
-    }
-    if (armNum==num){
-        printf("%d is Armstrong!!",num);
-    }
-    else{
-        printf("%d is not armstrong!!",num);
+    int limit;
+    printf("Enter the limit: ");
+    scanf("%d",&limit);
+    for (int num=1;num<=limit;num++){
+        int tempNum,length=0,armNum=0;
+        tempNum=num;
+        while (tempNum!=0){
+            length++;
+            tempNum/=10;
+        }
+        tempNum=num;
+        while (tempNum!=0){
+            int digit=tempNum%10;
+            armNum+=(pow(digit,length));
+            tempNum/=10;
+        }
+        if (armNum==num){
+            printf("%d is Armstrong!!\n",num);
+        }
+        else{
+            printf("%d is not armstrong!!\n",num);
+        }
     }
     return 0;
 }
