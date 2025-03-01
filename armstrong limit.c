70d15aaad252a72739472adcc90947992ed942e9
Dario George <edu.dariogeorge21@gmail.com>
2025-03-02 01:54:59 +0530

    Fixed

--- armstrong limit.c
+++ armstrong limit.c
@@ -5,20 +5,20 @@ int main() {
     int limit;
     printf("Enter the limit: ");
     scanf("%d",&limit);
-    for (int num=1;num<=limit;num++){
+    for (int num=1;num<=limit;num++){ // limit from 1 to number
         int tempNum,length=0,armNum=0;
         tempNum=num;
-        while (tempNum!=0){
+        while (tempNum!=0){ //checking the length
             length++;
             tempNum/=10;
         }
         tempNum=num;
-        while (tempNum!=0){
+        while (tempNum!=0){ //checking the armstrong
             int digit=tempNum%10;
             armNum+=(pow(digit,length));
             tempNum/=10;
         }
-        if (armNum==num){
+        if (armNum==num){ 
             printf("%d is Armstrong!!\n",num);
         }
         else{
