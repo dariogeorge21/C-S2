f46296e29a103cf36978c622ba88a63cd936be01
Dario George <edu.dariogeorge21@gmail.com>
2025-03-02 21:01:44 +0530

    Added!

--- star pattern.c
+++ star pattern.c
@@ -3,9 +3,23 @@
 int main(){
     int n;
     printf("Enter the number of rows: ");
-    scanf("%d", &n);
-    for(int i = 1; i <= n; i++){
+    scanf("%d", &n); 
+    for(int i = 1; i <= n; i++){ //floyd pattern or normal star pattern
         for(int j = 1; j <= i; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    for (int i = n-1; i >= 1; i--){ //reverse floyd pattern
+        for(int j = 1; j <= i; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    for (int i = 1; i <= n; i++){ //reverse star pattern
+        for(int j = n; j >= i; j--){
             printf("* ");
         }
         printf("\n");
