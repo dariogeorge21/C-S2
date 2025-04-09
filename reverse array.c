341ea4c163f0abda97f45c37ec489b0c0501574a
Dario George <edu.dariogeorge21@gmail.com>
2025-04-09 16:07:21 +0530

    reversing array

--- reverse array.c
+++ reverse array.c
@@ -1,23 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
-    int n;
-    printf("Enter the size of the array: ");
-    scanf("%d", &n);
-    int arr[n];
+    int num;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &num);
+    int arr[num];
     printf("Enter the elements of the array: ");
-    for(int i=0; i<n; i++){
+    for(int i=0; i<num; i++){
         scanf("%d", &arr[i]);
     }
     printf("The original array is: ");
-    for(int i=0; i<n; i++){
+    for(int i=0; i<num; i++){
         printf("%d ", arr[i]);
     }
     printf("\n");
+
+    for (int i=0;i<num/2;i++){
+        int temp = arr[i];
+        arr[i] = arr[num-i-1];
+        arr[num-i-1] = temp;
+    }
+
     printf("The reversed array is: ");
-    for(int i=n-1; i>=0; i--){
+    for(int i=0; i<num; i++){
         printf("%d ", arr[i]);
     }
     printf("\n");
+
     return 0;
 }
