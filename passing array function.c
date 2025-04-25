933c58aeb05b28824239a1e65d26ebfdc6aa19da
Dario George <edu.dariogeorge21@gmail.com>
2025-04-25 12:00:26 +0530

    func learn

---
+++ passing array function.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int pass(int[],int);
+
+int main(){
+    int a[5]={1,2,3,4,5};
+    int result=pass(a,5);
+    printf("sum of array is %d\n",result);
+    return 0;
+}
+
+int pass(int x[],int size){
+    int sum=0;
+    for (int i=0;i<size;i++){
+        sum=sum+x[i];
+    }
+    return sum;
+}
