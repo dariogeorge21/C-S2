549d3b1c36313aa3c8ff48cb217574ba8c9406f1
Dario George <edu.dariogeorge21@gmail.com>
2025-04-25 12:28:06 +0530

    learn mod3

---
+++ macro cube.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define CUBE(x) ((x)*(x)*(x))  // Added parentheses for operator precedence
+
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Cube of %d is %d\n", num, CUBE(num));  // Added newline, changed to uppercase
+    return 0;
+}
+
