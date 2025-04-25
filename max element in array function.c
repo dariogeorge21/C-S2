91c914d22c55615fab52639c8b84b380ca1ceb6a
Dario George <edu.dariogeorge21@gmail.com>
2025-04-25 12:17:56 +0530

    learn func

---
+++ max element in array function.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int max(int arr[],int n);
+
+int main(){
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    printf("The maximum element in the array is %d",max(arr,n));
+    return 0;
+}
+
+int max(int arr[],int n){
+    int max=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    return max;
+}
