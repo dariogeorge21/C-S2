0c154dfdef6101591cd4d29aa5c927096c7b4ac1
Dario George <edu.dariogeorge21@gmail.com>
2025-04-25 13:37:55 +0530

    struct learn

---
+++ structure students.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+struct student{
+    char name[20];
+    int roll;
+    int marks;
+};
+
+void printStruct(struct student[],int);
+int main(){
+    struct student students[5];
+    for(int i=0;i<5;i++){
+        printf("Enter student %d name: ",i+1);
+        scanf("%s",students[i].name);
+        printf("Enter student %d roll: ",i+1);
+        scanf("%d",&students[i].roll);
+        printf("Enter student %d marks: ",i+1);
+        scanf("%d",&students[i].marks);
+    }
+    printStruct(students,5);
+    return 0;
+}
+
+void printStruct(struct student students[],int n){
+    for(int i=0;i<n;i++){
+        printf("Student %d name: %s\n",i+1,students[i].name);
+        printf("Student %d roll: %d\n",i+1,students[i].roll);
+        printf("Student %d marks: %d\n",i+1,students[i].marks);
+    }
+}
