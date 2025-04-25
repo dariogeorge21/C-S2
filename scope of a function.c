aa3e00a521e651017a0d0b5786ec913b0115bea8
Dario George <edu.dariogeorge21@gmail.com>
2025-04-25 11:48:11 +0530

    scope learn

---
+++ scope of a function.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int scope(int);
+
+int main(){
+    int x = 10;
+    printf("The value of x is %d\n", x);
+    scope(x);
+    printf("The value of x is %d\n", x);
+    return 0;
+}
+
+int scope(int x){
+    x = 20;
+    printf("The value of x is %d\n", x);
+    return 0;
+}
+
+// Output:
+// The value of x is 10
+// The value of x is 20
+// The value of x is 10
