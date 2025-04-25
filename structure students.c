6a10a578004cda6d28e390e567228277a6af7292
Dario George <edu.dariogeorge21@gmail.com>
2025-04-25 13:39:48 +0530

    fix

--- structure students.c
+++ structure students.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 
 struct student{
     char name[20];
@@ -12,17 +13,21 @@ int main(){
     for(int i=0;i<5;i++){
         printf("Enter student %d name: ",i+1);
         scanf("%s",students[i].name);
+        getchar();//clear buffer
         printf("Enter student %d roll: ",i+1);
         scanf("%d",&students[i].roll);
         printf("Enter student %d marks: ",i+1);
         scanf("%d",&students[i].marks);
+        getchar();//clear buffer
     }
+    printf("\nStudent Details:\n")
     printStruct(students,5);
     return 0;
 }
 
 void printStruct(struct student students[],int n){
     for(int i=0;i<n;i++){
+        printf("\nStudent %d:\n",i+1);
         printf("Student %d name: %s\n",i+1,students[i].name);
         printf("Student %d roll: %d\n",i+1,students[i].roll);
         printf("Student %d marks: %d\n",i+1,students[i].marks);
