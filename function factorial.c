120ca2ed7cc659562bca0682904c15361c9aeff4
Dario George <edu.dariogeorge21@gmail.com>
2025-04-25 11:41:52 +0530

    learn func

---
+++ function factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int factorial(int);
+
+int main(){
+    int n, result;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    result = factorial(n);
+    printf("Factorial of %d = %d", n, result);
+    return 0;
+}
+int factorial(int x){
+    int fact=1;
+    for (int i = 1; i <= x; i++){
+        fact = fact * i;
+    }
+    return fact;
+}
