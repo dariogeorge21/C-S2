440e90bca46f05a636a651582a38e75ec682cf60
Dario George <edu.dariogeorge21@gmail.com>
2025-05-18 20:43:23 +0530

    type1 pattern

---
+++ patterns/alphabet cons.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int limit;
+    printf("Enter limit: ");
+    scanf("%d",&limit);
+    char alpha='A';
+    for(int i=0;i<limit;i++){
+        for (int j=0;j<i+1;j++){
+            printf("%c ",alpha);
+            
+        }printf("\n");
+        alpha++;
+    }
+}
