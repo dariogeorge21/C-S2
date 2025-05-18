c0a65491ed5882c7f339cb4bf07b8ac70f91185e
Dario George <edu.dariogeorge21@gmail.com>
2025-05-18 11:59:13 +0530

    update

--- arrays/bubble sort.c
+++ arrays/bubble sort.c
@@ -13,18 +13,18 @@ int main(){
         printf("%d\t",arr[i]);
     }
     for (int i=0;i<limit;i++){
-        for (int j=0;j<(limit-i-1);j++){
-            if (arr[j]>arr[j+1]){
-                int temp=arr[j+1];
-                arr[j+1]=arr[j];
-                arr[j]=temp;
+        for(int j=0;j<(limit-i-1);j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
             }
         }
-    } printf("\nThe sorted array is: \n");
-    for (int i=0;i<limit;i++){
-        printf("%d\t",arr[i]);
     }
-    return 0;
+    printf("\nThe sorted array:\n");
+    for(int i=0;i<limit;i++){
+        printf("%d ",arr[i]);
+    }
 }
 
 /*
